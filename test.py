117615d89e3c331b8de0c38656a60a3db04afdeb
Ludmila <yefanovaludmila@gmail.com>
2018-07-29 16:45:54 +0300

    commit

---
+++ test.py
@@ -0,0 +1,16 @@
+import telepot
+import time
+from telepot.loop import MessageLoop
+
+TOKEN = '673300038:AAGRn7-ObHoL5PeubyLymZ_AtjjO5h1ejkc'
+bot = telepot.Bot(TOKEN)
+
+def handle(msg):
+    print(msg)
+    text = (msg['text'])
+    bot.sendMessage(msg['chat']['id'], text)
+
+MessageLoop(bot, handle).run_as_thread()
+
+# import logging
+print('I am ready to work ...')
